d0e17f27ed7184ff076d30793c9b2d0b16d70a52
Akashtheory <akashthawarewhy@gmail.com>
2025-11-09 02:11:04 +0530

    day 2 of js

--- JSKing/script.js
+++ JSKing/script.js
@@ -1,1 +1,21 @@
-console.log("Hello bhai");
+
+let name = "Akash"
+
+
+
+let age = String(25)
+
+
+
+let b = "50";
+
+
+let akash = ["akash", 25, 90.30, "Nagpur"]
+
+console.log(typeof age);
+console.log( age + b);
+
+console.log("Akash Thaware")
+
+
+
