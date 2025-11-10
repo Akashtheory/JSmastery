c730067753d16352c85e9fb1e46cd442a5a5ce5e
Akashtheory <akashthawarewhy@gmail.com>
2025-11-11 01:43:36 +0530

    IF ELSE 20 questions

--- JSKing/script.js
+++ JSKing/script.js
@@ -1,21 +1,285 @@
+//Q1 
+// let age = 9;
 
-let name = "Akash"
+// if(age >=18){
+//     console.log("Eligible to vote");
+    
+// }
+
+// else{
+//     console.log("Not ready to vote");
+    
+// }
+
+//Q2
+
+// let isStudent = false;
+
+// if(isStudent === true){
+//     console.log("Metro Discount Applied");
+    
+// }
+// else{
+//     console.log("No Discount");
+    
+// }
+
+//Q3
+
+
+// let temp = 10;
+
+// if(temp > 30){
+//     console.log("Garmi Lag rhi hai");
+    
+// }
+// else if(temp > 20){
+//     console.log("Normal hai");
+    
+// }
+// else{
+//     console.log("Thandi Lag Rahi Hai");
+    
+// }
+//Q4
+
+// let result = 32;
+
+// if(result >= 35){
+//     console.log("Pass ho gya");
+    
+// }
+// else if(result >30){
+//     console.log("Grace hai");
+    
+// }
+// else{
+//     console.log("Fail ho gya");
+    
+// }
+//Q5
+// let daysleft = 0;
+
+// if(daysleft > 0){
+//     console.log("Recharge hai");
+    
+// }
+// else{
+//     console.log("Recharge krwao");
+    
+// }
+//Q6
+// let battery = 20;
+
+// if(battery > 20){
+//     console.log("Battery hai");
+    
+// }
+// else{
+//     console.log("Battery low hai");
+    
+// }
+//Q7
+// let speed = 88;
+
+// let limit = 70;
+
+// if(speed > limit){
+//     console.log("OverSpeed Limit");
+    
+// }
+// else{
+//     console.log("Normal Speed hai");
+    
+// }
+
+//Q8
+
+// let age = 50;
+
+// if(age >= 60){
+//     console.log("Senior citizen consession");
+    
+// }
+// else{
+//     console.log("Regular Fare");
+    
+// }
+//Q9
+// let userOTP = "12345";
+//  let actualOTP = "12345";
+
+//  if(userOTP === actualOTP){
+//     console.log("Valid Password");
+    
+//  }
+//  else{
+//     console.log("Wrong Password");
+    
+//  }
+//Q10
+
+// let age = 42;
+
+// if(age <= 12){
+//     console.log("kID HAI");
+    
+// }
+// else{
+//     console.log("ADULT HAI");
+    
+// }
+ //Q12
+
+//  let target = 350;
+//   let india = 356;
+
+//   if(india >= target){
+//     console.log("Indian jeet gayi");
+    
+//   }
+//   else{
+//     console.log("Run baki hai");
+    
+//   }
+
+
+//Q13
+
+
+// let balance = 5000;
+
+// let withdraw = 6000;
+
+// if(withdraw >= balance){
+//     console.log("Cash Nhi hai ");
+    
+// }
+// else{
+//     console.log("Cash Hai");
+    
+// }
 
 
 
-let age = String(25)
+//Q16
+//  let usedGB = 2;
+//  let capGB = 2;
+
+
+//  if(usedGB >= capGB){
+//     console.log("Data CAP REACHED: SPEED REDUCED");
+    
+//  }
+
+//  else{
+//     console.log("High Speed Active");
+    
+//  }
+
+//Q20
+// let hasCoupon = true;
+//  let ammount = 8000;
+
+//  if(hasCoupon === true && ammount >= 1000){
+//     console.log("Coupon Applied");
+    
+//  }
+//  else{
+//     console.log("Coupon not applied");
+    
+//  }
+
+
+// Project := Marksheet // grades(A,B,C,D, Grace & Fail)
+
+let hindi = 58;
+
+let english = 65;
+
+let maths = 71;
+
+let science = 90;
+
+let Social = 65;
+
+let total = hindi + english + maths + science + Social;
+
+
+let percentage = (total /500)*100;
+
+
+console.log("Total Marks:"+ total);
+
+console.log("Percentage:" + percentage + "%");
+
+
+// Grades
+
+if(percentage >= 85 && percentage<=100){
+    console.log("Grade A+");
+    
+}
+else if(percentage >= 75 && percentage <= 85){
+    console.log("Grade A");
+    
+}
+else if(percentage >= 60 && percentage <= 75){
+    console.log("Grade B");
+    
+}
+else if(percentage >= 45 && percentage <= 60){
+    console.log("Grade C");
+    
+}
+else if(percentage >= 33 && percentage <= 45){
+    console.log("Grade D");
+    
+}
+else if(percentage >= 29 && percentage <= 33){
+    console.log("Grace mili hai");
+    
+}
+else if(percentage >= 0 && percentage <= 28){
+    console.log("Fail Ho Gya");
+    
+}
+else{
+    console.log("Absent Fail Ho Gya");
+    
+}
 
 
 
-let b = "50";
 
 
-let akash = ["akash", 25, 90.30, "Nagpur"]
 
-console.log(typeof age);
-console.log( age + b);
 
-console.log("Akash Thaware")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
 
 
 
