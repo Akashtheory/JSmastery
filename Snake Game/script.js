66b13ad82130d6748c455632e1e90d09770bc54a
Akashtheory <akashthawarewhy@gmail.com>
2025-11-21 14:58:18 +0530

    Easy control with key in four direction

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -10,13 +10,9 @@ const rows = Math.floor(board.clientHeight / blockHeight);
 const blocks = [];
 const snake = [{
     x: 1, y: 3
-}, {
-    x: 1, y: 4
-}, {
-    x: 1, y: 5
 }]
 
-let direction = 'left';
+let direction = 'down';
 
 // for(let i =0; i < rows * cols; i++){
 //     const block = document.createElement('div');
@@ -46,12 +42,41 @@ setInterval(() => {
     if (direction === 'left'){
         head = {x:snake[0].x, y:snake[0].y - 1} 
     }
+    else if(direction === 'right'){
+          head = {x:snake[0].x, y:snake[0].y + 1}
+    }
+    else if(direction === 'down'){
+        head = {x:snake[0].x+ 1, y:snake[0].y}
+    }
+    else if (direction === 'up'){
+        head = {x:snake[0].x-1, y:snake[0].y}
+    }
+
+
     snake.forEach(segment => {
         blocks[`${segment.x}-${segment.y}`].classList.remove('fill') 
     })
-    snake.unshift(head)
-    snake.pop()
+     snake.unshift(head)
+     snake.pop()
 
 
     render()
 }, 600)
+
+
+
+
+addEventListener('keydown', (event)=>{
+    if(event.key == "ArrowUp"){
+        direction = "up";
+    }
+    else if(event.key == "ArrowDown"){
+        direction = "down";
+    }
+    else if(event.key == "ArrowRight"){
+        direction = "right";
+    }
+    else if(event.key == "ArrowLeft"){
+        direction = "left";
+    }
+})
