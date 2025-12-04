5eab5899911cd91932e72e5f07abbf92d458cce6
Akashtheory <akashthawarewhy@gmail.com>
2025-12-04 18:44:45 +0530

    Text Change

--- JSProject1/script.js
+++ JSProject1/script.js
@@ -1,14 +1,21 @@
 var istatus = document.querySelector("h5");
 
 var addFreind = document.querySelector("#add");
-var removeFriend = document.querySelector("#remove");
+var check = 0;
 
 addFreind.addEventListener("click", function(){
+    if(check == 0){
     istatus.innerHTML = "Buddy"
     istatus.style.color = "rgba(142, 255, 13, 1)"
+    
+    check = 1
+    }
+
+    else{
+    istatus.innerHTML = "Pokemon Trainer"
+    istatus.style.color = "rgba(13, 69, 255, 1)"
+    
+    check = 0
+    }
 })
 
-removeFriend.addEventListener("click", function(){
-    istatus.innerHTML = "Pokemon Trainer"
-    istatus.style.color = "rgba(13, 78, 255, 1)"
-})
