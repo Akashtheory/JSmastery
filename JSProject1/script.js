798d8746516fe18e0b6e755e979db16b54b71dea
Akashtheory <akashthawarewhy@gmail.com>
2025-12-04 18:49:18 +0530

    First Task Done

--- JSProject1/script.js
+++ JSProject1/script.js
@@ -1,20 +1,22 @@
 var istatus = document.querySelector("h5");
 
-var addFreind = document.querySelector("#add");
+var btn = document.querySelector("#add");
 var check = 0;
 
-addFreind.addEventListener("click", function(){
+btn.addEventListener("click", function(){
     if(check == 0){
     istatus.innerHTML = "Buddy"
     istatus.style.color = "rgba(142, 255, 13, 1)"
-    
+    btn.innerHTML = "Remove"
+    btn.style.backgroundColor = "#dadada"
     check = 1
     }
 
     else{
     istatus.innerHTML = "Pokemon Trainer"
     istatus.style.color = "rgba(13, 69, 255, 1)"
-    
+    btn.innerHTML = "Add Freind"
+    btn.style.backgroundColor = "#4db8ffff"
     check = 0
     }
 })
