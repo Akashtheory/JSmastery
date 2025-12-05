0c72f7b77b72eca4cca2139c769d53a864646245
Akashtheory <akashthawarewhy@gmail.com>
2025-12-05 19:15:01 +0530

    New Card in JSproject2

--- JSProject2/script.js
+++ JSProject2/script.js
@@ -1,5 +1,7 @@
 var con = document.querySelector("#container");
+var con1 = document.querySelector("#container1");
 var icon = document.querySelector("i");
+var love = document.querySelector("#emoji");
 
 con.addEventListener("dblclick", function(){
     icon.style.transform = 'translate(-50%, -50%) scale(1)';
@@ -11,6 +13,20 @@ con.addEventListener("dblclick", function(){
     
     setTimeout(function(){
      icon.style.transform = 'translate(-50%, -50%) scale(0)';   
+    }, 2000)
+    
+})
+
+con1.addEventListener("dblclick", function(){
+    emoji.style.transform = 'translate(-50%, -50%) scale(1)';
+    emoji.style.opacity = 0.8;
+
+    setTimeout(function(){
+     emoji.style.opacity =0;   
+    }, 1000)
+    
+    setTimeout(function(){
+     emoji.style.transform = 'translate(-50%, -50%) scale(0)';   
     }, 2000)
     
 })
